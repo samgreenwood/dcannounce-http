be4b9e424ac21ce248e50c9a513055516619aad0
Sam Greenwood <sam@samgreenwood.me>
2015-02-11 00:37:29 +1030

    Move human readable helper to app.php

--- app.php
+++ app.php
@@ -20,7 +20,22 @@ $createTableIfNotExists = "CREATE TABLE IF NOT EXISTS announcements(
 
 $pdo->prepare($createTableIfNotExists)->execute();
 
+function humanFileSize($size)
+{
+   if ($size >= 1073741824) {
+      $fileSize = round($size / 1024 / 1024 / 1024, 1) . 'GB';
+   } elseif ($size >= 1048576) {
+      $fileSize = round($size / 1024 / 1024, 1) . 'MB';
+   } elseif ($size >= 1024) {
+      $fileSize = round($size / 1024, 1) . 'KB';
+   } else {
+      $fileSize = $size . ' bytes';
+   }
+   return $fileSize;
+}
+
 $app = new Slim(['templates.path' => '../templates']);
+
 require 'routes.php';
 
 $app->run();
--- routes.php
+++ routes.php
@@ -47,17 +47,3 @@ $app->post('/announce', function () use ($app, $announcementRepository) {
     $response->finalize();
 
 });
-
-function humanFileSize($size)
-{
-    if ($size >= 1073741824) {
-        $fileSize = round($size / 1024 / 1024 / 1024, 1) . 'GB';
-    } elseif ($size >= 1048576) {
-        $fileSize = round($size / 1024 / 1024, 1) . 'MB';
-    } elseif ($size >= 1024) {
-        $fileSize = round($size / 1024, 1) . 'KB';
-    } else {
-        $fileSize = $size . ' bytes';
-    }
-    return $fileSize;
-}
